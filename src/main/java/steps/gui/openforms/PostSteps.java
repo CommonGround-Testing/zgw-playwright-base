f589ce76b8c3d1a7c70d428ef213baf176db98c8
FrederikJaapCampagne <frederik.campagne@denhaag.nl>
2024-02-22 21:09:59 +0100

    post steps

---
+++ src/main/java/steps/gui/openforms/PostSteps.java
@@ -0,0 +1,10 @@
+package steps.gui.openforms;
+
+import com.microsoft.playwright.Page;
+
+public class PostSteps extends OpenFormsSteps {
+
+    public PostSteps(Page page) {
+        super(page);
+    }
+}
