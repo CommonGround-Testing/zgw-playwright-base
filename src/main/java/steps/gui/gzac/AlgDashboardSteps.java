b3d724025270df1d8981edf7afcc658dd6fa482b
Lampie1973 <63862196+lampie1973@users.noreply.github.com>
2024-03-19 09:07:46 +0100

    Renamed class

--- src/main/java/steps/gui/gzac/AlgDashboardSteps.java
+++ src/main/java/steps/gui/gzac/AlgDashboardSteps.java
@@ -1,15 +1,15 @@
 package steps.gui.gzac;
 
 import com.microsoft.playwright.Page;
-import pages.gzac.GzacALGDashboardPage;
+import pages.gzac.GzakDashboardPage;
 
 public class AlgDashboardSteps extends GzacBaseSteps {
 
-    protected final GzacALGDashboardPage dashboardPage;
+    protected final GzakDashboardPage dashboardPage;
 
     public AlgDashboardSteps(Page page) {
         super(page);
-        this.dashboardPage = new GzacALGDashboardPage(page);
+        this.dashboardPage = new GzakDashboardPage(page);
     }
 
     public void navigate() {
