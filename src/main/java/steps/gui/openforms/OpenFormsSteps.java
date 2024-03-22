7eac58a9ede47f166f88a1d3abaf600c558f622b
FrederikJaapCampagne <frederik.campagne@denhaag.nl>
2024-03-22 17:14:41 +0100

    eerste probeersel

--- src/main/java/steps/gui/openforms/OpenFormsSteps.java
+++ src/main/java/steps/gui/openforms/OpenFormsSteps.java
@@ -19,13 +19,12 @@ public class OpenFormsSteps extends GeneriekeSteps {
     public OpenFormsSteps(Page page) {
         super(page);
         openFormsPage = new OpenFormsPage(page);
-        genericPage  = new GeneriekeOpenformsPage(page);
+        genericPage = new GeneriekeOpenformsPage(page);
         digidLoginSteps = new DigidLoginSteps(page);
     }
 
     /**
      * Open de pagina voor aanvraag ooievaarspas
-     *
      */
     public void open_pagina() {
         page.navigate("/aanvraag-formulier-ooievaarspas/startpagina");
@@ -59,6 +58,7 @@ public class OpenFormsSteps extends GeneriekeSteps {
 
     /**
      * Valideer dat een bepaalde h1 header op het scherm staat
+     *
      * @param tekst
      */
     public void controleer_h1_header_is(String tekst) {
@@ -68,6 +68,7 @@ public class OpenFormsSteps extends GeneriekeSteps {
 
     /**
      * Valideer dat een bepaalde h2 header op het scherm staat
+     *
      * @param tekst
      */
     public void controleer_h2_header_is(String tekst) {
@@ -89,7 +90,7 @@ public class OpenFormsSteps extends GeneriekeSteps {
      * @param tekst
      * @return Locator waarop een actie uitgevoerd kan worden
      */
-    public Locator get_tekst(String tekst){
+    public Locator get_tekst(String tekst) {
         return page.getByText(tekst);
     }
 
@@ -99,8 +100,8 @@ public class OpenFormsSteps extends GeneriekeSteps {
      * @param tekst
      * @return Locator waarop een actie uitgevoerd kan worden
      */
-    public Locator get_input_veld(String tekst){
-        return genericPage.getInputField(tekst, false);
+    public Locator get_input_veld(String tekst) {
+        return openFormsPage.getInputField(tekst, false);
     }
 
     /**
@@ -108,15 +109,18 @@ public class OpenFormsSteps extends GeneriekeSteps {
      *
      * @return Locator waarop een actie uitgevoerd kan worden
      */
-    public Locator get_alert(){
+    public Locator get_alert() {
         return page.locator("//div[contains(@role,'alert')]//div");
     }
 
     /**
      * Valideer dat er geen foutmelding zichtbaar is op het scherm
-     *
      */
-    public void geen_foutmelding_zichtbaar() {
+    public void geen_foutmelding_zichtbaar(Locator locator) {
+        locator.blur();
+        locator.waitFor(new Locator.WaitForOptions().setTimeout(200));
+        page.keyboard().press("Enter");
+        page.keyboard().press("Tab");
         assertThat(page.locator("//div[@class='form-text error']")).isHidden();
     }
 
@@ -127,7 +131,7 @@ public class OpenFormsSteps extends GeneriekeSteps {
      * @param prefillWaarde
      */
     public void tekstveld_bevat_prefill_gegevens
-            (String inputName, String prefillWaarde) {
+    (String inputName, String prefillWaarde) {
         assertThat(
                 page.locator(
                         String.format("//input[@name='%s' and contains(@value,\"%s\")]",
@@ -147,16 +151,11 @@ public class OpenFormsSteps extends GeneriekeSteps {
         locator.waitFor(new Locator.WaitForOptions().setTimeout(1000));
         page.keyboard().press("Enter");
         page.keyboard().press("Tab");
-        if (verwachteTekst == null) {
-            geen_foutmelding_zichtbaar();
-        } else {
-            controleer_foutmelding_is_zichtbaar_met(verwachteTekst);
-        }
+        controleer_foutmelding_is_zichtbaar_met(verwachteTekst);
     }
 
     /**
      * Klik op de volgende knop
-     *
      */
     public void klik_volgende_knop() {
         wacht_op_volgende_knop_response();
@@ -165,12 +164,12 @@ public class OpenFormsSteps extends GeneriekeSteps {
 
     /**
      * Wacht totdat de validatie in de backend is uitgevoerd
-     *
      */
-    public void wacht_op_volgende_knop_response(){
-        try{
+    public void wacht_op_volgende_knop_response() {
+        try {
             page.setDefaultTimeout(2000);
-            page.waitForResponse(res -> res.url().contains("/_check_logic"), () -> {});
+            page.waitForResponse(res -> res.url().contains("/_check_logic"), () -> {
+            });
         } catch (Exception ex) {
             //ignore
         } finally {
@@ -184,8 +183,8 @@ public class OpenFormsSteps extends GeneriekeSteps {
      * @param tekst
      * @return Locator waarop een actie uitgevoerd kan worden
      */
-    public Locator get_checkbox(String tekst){
-        return genericPage.getCheckbox(tekst);
+    public Locator get_checkbox(String tekst) {
+        return openFormsPage.getCheckbox(tekst);
     }
 
     /**
@@ -195,18 +194,34 @@ public class OpenFormsSteps extends GeneriekeSteps {
      * @param optie die geselecteerd moet worden
      */
     public void selecteer_optie(String tekst, String optie) {
-        genericPage.selectOption(tekst, optie);
+        openFormsPage.selectOption(tekst, optie);
     }
 
     /**
-     *
      * Note that this only works on input and textarea fields
      *
      * @param veld - text of the field you want to enter the text for
      * @param text - text you want to enter into the field
      */
     public void vul_tekst_in(String veld, String text) {
-        genericPage.getInputField(veld, false).waitFor(new Locator.WaitForOptions().setTimeout(500));
-        genericPage.fillTextInputField(veld, text, false);
+        openFormsPage.getInputField(veld, false).waitFor(new Locator.WaitForOptions().setTimeout(500));
+        openFormsPage.fillTextInputField(veld, text, false);
+    }
+
+    public void test_foutmelding_voor_veld(String veld, String tekst, String verwachteMelding) {
+        Locator inputField = fillFieldAndWaitForValidation(veld, tekst);
+        validatie_toon_foutmelding(inputField, verwachteMelding);
+    }
+
+    private Locator fillFieldAndWaitForValidation(String veld, String tekst) {
+        var inputField = openFormsPage.getInputField(veld, false);
+        openFormsPage.fillTextInputField(veld, tekst, false);
+        wacht_op_volgende_knop_response();
+        return inputField;
+    }
+
+    public void test_geen_foutmelding_voor_veld(String veld, String tekst) {
+        Locator inputField = fillFieldAndWaitForValidation(veld, tekst);
+        geen_foutmelding_zichtbaar(inputField);
     }
 }
