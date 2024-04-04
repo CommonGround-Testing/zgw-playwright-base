78a5cf632a1147e906f2a283418d23409700efc5
Lampie1973 <63862196+lampie1973@users.noreply.github.com>
2024-04-04 08:32:57 +0200

    Fixed erfpachttesten

--- src/main/java/pages/klantportaal/ErfpachtPage.java
+++ src/main/java/pages/klantportaal/ErfpachtPage.java
@@ -5,26 +5,28 @@ import com.microsoft.playwright.Page;
 
 public class ErfpachtPage {
 
-    private final Page page;
     private final static String h3Header = "//h3[contains(text(),'${text}')]";
-    private final static String contractsPath = "/../..//tbody/tr";
-    private final String takenPath = "/../..//a";
-    public final Locator zakenLocator;
+    private final Page page;
 
     public ErfpachtPage(Page page) {
         this.page = page;
-        zakenLocator = page.locator("//div[@class='denhaag-card__content']");
     }
 
-    public Locator getContracten(String text){
+    public Locator getContracten(String text) {
         var headerPath = h3Header.replace("${text}", text);
-        var fullPath = headerPath + contractsPath;
+        var fullPath = headerPath + "/../..//tbody/tr";
         return page.locator(fullPath);
     }
 
-    public Locator getTaken(String text){
+    public Locator getTaken(String text) {
         var headerPath = h3Header.replace("${text}", text);
-        var fullPath = headerPath + takenPath;
+        var fullPath = headerPath + "/../..//a";
+        return page.locator(fullPath);
+    }
+
+    public Locator getZaken(String text) {
+        var headerPath = h3Header.replace("${text}", text);
+        var fullPath = headerPath + "/../../div//a";
         return page.locator(fullPath);
     }
 }
