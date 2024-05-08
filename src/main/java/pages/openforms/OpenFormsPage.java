6844f63e300cdd3c5658b834dc3e47758fd34f00
Rocky Kuang <shrkuang@gmail.com>
2024-05-08 14:58:54 +0200

    scope: refactor digidLogin
    - removed open_pagina()
    - changed waitForVisible state from textlabelheader to logout button.
    - restructured OpenFormsPage elements

--- src/main/java/pages/openforms/OpenFormsPage.java
+++ src/main/java/pages/openforms/OpenFormsPage.java
@@ -5,22 +5,30 @@ import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
 
 public class OpenFormsPage {
+    public final Locator inloggenDigidButton;
+    public final Locator uitloggenDigidButton;
+    public final Locator aanvraagZonderDigidButton;
 
+    public final Locator cookieButton;
     public final Locator buttonAccepteerCookies;
+
     public final Locator linkActiveStep;
     public final Locator textlabelHeaderH1;
     public final Locator textlabelHeaderH2;
-    public final Locator inloggenDigidButton;
-    public final Locator cookieButton;
     public final Locator headerFirstFormStep;
 
+
     public OpenFormsPage(Page page) {
+        this.inloggenDigidButton = page.locator("//button[text()='Inloggen']");
+        this.uitloggenDigidButton = page.locator("//button[text()='Uitloggen']");
+        this.aanvraagZonderDigidButton = page.locator("//button[text()='Aanvragen zonder DigiD']");
+
+        this.cookieButton = page.locator("//form[@action='/cookies/accept/']/button");
         buttonAccepteerCookies = page.getByRole(AriaRole.BUTTON).locator(":scope.cookie-notice__accept");
+
         linkActiveStep = page.getByRole(AriaRole.LINK).locator(":scope.utrecht-link--openforms-active");
         textlabelHeaderH1 = page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Ooievaarspas aanvragen"));
         textlabelHeaderH2 = page.locator("//h2").first();
-        this.inloggenDigidButton = page.locator("//a[contains(@class,'utrecht-button-link--primary-action')]");
-        this.cookieButton = page.locator("//form[@action='/cookies/accept/']/button");
         this.headerFirstFormStep = page.locator("//h2[contains(text(),'Inleiding')]");
     }
 }
