2c92ba8c991847df19e9888fcc5c9b7ec705a595
Rocky <shrkuang@gmail.com>
2024-05-14 13:22:37 +0200

    scope: make login and logout button more dynamic in OpenFormsPage
    - refactored inloggeDigidButton uitloggenDigidbutton and aanvraagzonderDigidbutton

--- src/main/java/pages/openforms/OpenFormsPage.java
+++ src/main/java/pages/openforms/OpenFormsPage.java
@@ -6,28 +6,36 @@ import com.microsoft.playwright.options.AriaRole;
 
 public class OpenFormsPage {
     public final Locator inloggenDigidButton;
+
     public final Locator uitloggenDigidButton;
+
     public final Locator aanvraagZonderDigidButton;
 
     public final Locator cookieButton;
+
     public final Locator buttonAccepteerCookies;
 
     public final Locator linkActiveStep;
+
     public final Locator textlabelHeaderH1;
+
     public final Locator textlabelHeaderH2;
+
     public final Locator headerFirstFormStep;
 
 
     public OpenFormsPage(Page page) {
         this.inloggenDigidButton = page.getByRole(AriaRole.BUTTON).getByText("Inloggen met DigiD");
-        this.uitloggenDigidButton = page.locator("//button[text()='Uitloggen']");
-        this.aanvraagZonderDigidButton = page.locator("//button[text()='Aanvragen zonder DigiD']");
+        this.uitloggenDigidButton = page.getByRole(AriaRole.BUTTON).getByText("Uitloggen");
+        this.aanvraagZonderDigidButton = page.getByRole(AriaRole.BUTTON).getByText("zonder DigiD");
 
         this.cookieButton = page.locator("//form[@action='/cookies/accept/']/button");
-        buttonAccepteerCookies = page.getByRole(AriaRole.BUTTON).locator(":scope.cookie-notice__accept");
+        buttonAccepteerCookies = page.getByRole(AriaRole.BUTTON).locator(
+                ":scope.cookie-notice__accept");
 
         linkActiveStep = page.getByRole(AriaRole.LINK).locator(":scope.utrecht-link--openforms-active");
-        textlabelHeaderH1 = page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Ooievaarspas aanvragen"));
+        textlabelHeaderH1 = page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().
+                setName("Ooievaarspas aanvragen"));
         textlabelHeaderH2 = page.locator("//h2").first();
         this.headerFirstFormStep = page.locator("//h2[contains(text(),'Inleiding')]");
     }
