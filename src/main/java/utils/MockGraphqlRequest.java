419eff30ec8f6da5e99cc48458140fa074be309c
Erwin <63862196+lampie1973@users.noreply.github.com>
2024-06-14 10:28:02 +0200

    Multimock graphql (#59)
    * added new method for Mocking multiple graphql requests
    
    * v0.0.62

--- src/main/java/utils/MockGraphqlRequest.java
+++ src/main/java/utils/MockGraphqlRequest.java
@@ -2,9 +2,13 @@ package utils;
 
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Route;
+import org.json.JSONObject;
 import runner.ZGWTestRunner;
 
 import java.io.IOException;
+import java.util.Map;
+
+import static runner.ZGWTestRunner.page;
 
 public class MockGraphqlRequest{
 
@@ -13,9 +17,9 @@ public class MockGraphqlRequest{
      * The path to the json file should contain the full path to the file starting from src
      *  example: src/test/resources/fileWithMockData.json
      *
-     * @param page
-     * @param query
-     * @param pathToMockJson
+     * @param page with Page
+     * @param query with string of graphql payload operationName which is used for matching the request
+     * @param pathToMockJson with string of the full path to the json file (example: src/test/resources/fileWithMockData.json)
      */
     public static void mockRequestWithQuery(Page page, String query, String pathToMockJson) {
         page.route(ZGWTestRunner.getBaseUrl() + "/graphql", route -> {
@@ -26,6 +30,31 @@ public class MockGraphqlRequest{
             }
         });
     }
+
+    /**
+     * @param subDirectory with the subdirectory of the response folder
+     *                     by default the subDirectory should be in the src/test/resources/
+     * @param queries   with the Map
+     *                  The map should contain the following
+     *                  - Key with the query parameter of the graphql
+     *                  - Value with the json file containing the reponse
+     */
+    public static void mockMultipleGraphql(String subDirectory, Map<String, String> queries) {
+        page.route(ZGWTestRunner.getBaseUrl() + "/graphql", route -> {
+            var operationName = new JSONObject(route.request().postData()).get("operationName");
+            if (!queries.containsKey((String)operationName)){
+                route.resume();
+                return;
+            }
+            try {
+                route.fulfill(new Route.FulfillOptions()
+                        .setBody(FiletoJson.convert("src/test/resources/" + subDirectory + "/" + queries.get((String)operationName))));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
 
     private static void mockResponseWithDataInJson(String pathToMockJson, Route route) {
         try {
